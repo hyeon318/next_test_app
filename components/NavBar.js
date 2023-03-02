2d937b7b274df2cc0d511f477117308caea483f6
spidey <kyurin123@gmail.com>
2023-03-02 14:37:58 +0900

    next.config.js 설정

--- components/NavBar.js
+++ components/NavBar.js
@@ -10,13 +10,43 @@ const NavBar = () => {
     <nav>
       {/* <a href="/">Home</a> 를 쓰는경우 페이지 자체라 reload 되어 spa 적으로 동작하지 않는다 => Link tag 를 사용할 것*/}
       {/* Link tag는 client side 의 navigation 을 생성해줌 */}
-      <Link href="/" legacyBehavior >
-        <a className={router.pathname === "/" ? "active" : ""}>Home</a>
-      </Link>
-      <Link href="/about" legacyBehavior >
-        <a className={router.pathname === "/about" ? "active" : ""}>About</a>
-      </Link>
-      <style jsx>{`a{text-decoration: none;} .active{color:tomato;}`}</style>
+      <img src="/vercel.svg" alt="Landscape Picture" /> {/* public/* 경로여서 */}
+      <div>
+        <Link href="/" legacyBehavior>
+          <a className={router.pathname === "/" ? "active" : ""}>Home</a>
+        </Link>
+        <Link href="/about" legacyBehavior>
+          <a className={router.pathname === "/about" ? "active" : ""}>About</a>
+        </Link>
+      </div>
+      <style jsx>{`
+        nav {
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+        }
+        Image {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
+        }
+        .active {
+          color: tomato;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
+        }
+
+      `}</style>
     </nav>
   );
 };
--- next.config.js
+++ next.config.js
@@ -1,6 +1,32 @@
 /** @type {import('next').NextConfig} */
+
+const API_KEY = process.env.API_KEY
+
 const nextConfig = {
   reactStrictMode: true,
+  async redirects(){
+    return [
+      {
+        source : "/contract",
+        destination: "https://www.naver.com",
+        permanent: false
+      },
+      {
+        source : "/old-blog/:path",
+        destination: "/new-blog/:path",
+        //  때에 따라서 :path* 를 쓰면 이후 path에서도 해당 param을 유지함
+        permanent: false
+      }
+    ]
+  },
+  async rewrites(){
+    return [{
+      source: "/api/movies",
+      destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+    }]
+  }
 }
-
+// redirect 옵션 
+// http://localhost:3000/contract ==redirect==> https://www.naver.com
+// http://localhost:3000/old-blog/123 ==redirect==> http://localhost:3000/new-blog/123
 module.exports = nextConfig
