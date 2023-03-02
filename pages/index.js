edcf906c3a3e0a00ddd662139c954b51f4d50457
spidey <kyurin123@gmail.com>
2023-03-03 02:14:42 +0900

    영화 상세페이지 link 연결

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import NavBar from '@/components/NavBar';
+import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import Seo from './Seo';
 // import '../styles/globals.css'; //동작 x
@@ -7,26 +8,21 @@ import Seo from './Seo';
 const API_KEY = "10923b261ba94d897ac6b81148314a3f";
 // api key 숨기기
 
-export default function Home() {
-  const [movies, setMovies] = useState();
-  useEffect(() => {
-    (async () => {
-      const { results } = await (
-        await fetch( `/api/movies`)
-      ).json();
-       // `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-      setMovies(results);
-    })();
-  }, []);
+export default function Home({results}) {
   return (
     <div className="container">
       <Seo title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie) => (
-        <div className="movie" key={movie.id}>
-        <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-          <h4>{movie.original_title}</h4>
-        </div>
+      {/* pre-rendering */}
+      {/* view-source:http://localhost:3000/ */}
+      {/* use effect 이후로 data 를 loading 한 후에 실제 데이터를 가져온다. */}
+      {!results && <h4>Loading...</h4>}
+      {results?.map((movie) => (
+        <Link href={`/movies/${movie.id}`} key={movie.id} legacyBehavior>
+          <div className="movie" key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+            <h4>{movie.original_title}</h4>
+          </div>
+        </Link>
       ))}
       <style jsx>{`
         .container {
@@ -51,4 +47,22 @@ export default function Home() {
       `}</style>
     </div>
   );
+}
+
+// export 필수 function name 도 고정
+// running server
+export async function getServerSideProps(){
+  // only going to server not client
+  // not show to client
+// dosen't works because /api/movies is front url
+// so, http://localhost:3000 으로 변경
+  console.log("getserverprops")
+  // {/* view-source:http://localhost:3000/ */} 기존에 loading... 창에서 server side에서 data 까지 rendering 해서 return
+  const {results} = await (await fetch(`http://localhost:3000/api/movies`)).json();
+  console.log(results);
+  return{
+    props: {
+      results
+    }
+  }  
 }
